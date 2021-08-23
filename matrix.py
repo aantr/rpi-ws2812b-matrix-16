ab6a28662e9ff65a7b0e3d381869d56818c2c212
aantr <xgamestudio8@gmail.com>
2021-08-23 13:34:28 +0500

    #

--- matrix.py
+++ matrix.py
@@ -165,10 +165,16 @@ class Matrix:
 
     @staticmethod
     def get_local_ip():
+        print([l for l in (
+            [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [
+                [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in
+                 [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l])
         for addr in getIPAddresses():
             addr = addr.decode('utf8')
             if Matrix.check_format_ip(addr):
                 return addr
+
+
 
         raise ValueError('Cannot determine local ip address')
         local_host = []

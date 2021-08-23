d0154f54db83caaa988e93e90cb89a00b8bb2207
aantr <xgamestudio8@gmail.com>
2021-08-23 13:30:51 +0500

    #

--- matrix.py
+++ matrix.py
@@ -5,6 +5,62 @@ import time
 import requests
 import subprocess as sp
 
+def getIPAddresses():
+    from ctypes import Structure, windll, sizeof
+    from ctypes import POINTER, byref
+    from ctypes import c_ulong, c_uint, c_ubyte, c_char
+    MAX_ADAPTER_DESCRIPTION_LENGTH = 128
+    MAX_ADAPTER_NAME_LENGTH = 256
+    MAX_ADAPTER_ADDRESS_LENGTH = 8
+    class IP_ADDR_STRING(Structure):
+        pass
+    LP_IP_ADDR_STRING = POINTER(IP_ADDR_STRING)
+    IP_ADDR_STRING._fields_ = [
+        ("next", LP_IP_ADDR_STRING),
+        ("ipAddress", c_char * 16),
+        ("ipMask", c_char * 16),
+        ("context", c_ulong)]
+    class IP_ADAPTER_INFO (Structure):
+        pass
+    LP_IP_ADAPTER_INFO = POINTER(IP_ADAPTER_INFO)
+    IP_ADAPTER_INFO._fields_ = [
+        ("next", LP_IP_ADAPTER_INFO),
+        ("comboIndex", c_ulong),
+        ("adapterName", c_char * (MAX_ADAPTER_NAME_LENGTH + 4)),
+        ("description", c_char * (MAX_ADAPTER_DESCRIPTION_LENGTH + 4)),
+        ("addressLength", c_uint),
+        ("address", c_ubyte * MAX_ADAPTER_ADDRESS_LENGTH),
+        ("index", c_ulong),
+        ("type", c_uint),
+        ("dhcpEnabled", c_uint),
+        ("currentIpAddress", LP_IP_ADDR_STRING),
+        ("ipAddressList", IP_ADDR_STRING),
+        ("gatewayList", IP_ADDR_STRING),
+        ("dhcpServer", IP_ADDR_STRING),
+        ("haveWins", c_uint),
+        ("primaryWinsServer", IP_ADDR_STRING),
+        ("secondaryWinsServer", IP_ADDR_STRING),
+        ("leaseObtained", c_ulong),
+        ("leaseExpires", c_ulong)]
+    GetAdaptersInfo = windll.iphlpapi.GetAdaptersInfo
+    GetAdaptersInfo.restype = c_ulong
+    GetAdaptersInfo.argtypes = [LP_IP_ADAPTER_INFO, POINTER(c_ulong)]
+    adapterList = (IP_ADAPTER_INFO * 10)()
+    buflen = c_ulong(sizeof(adapterList))
+    rc = GetAdaptersInfo(byref(adapterList[0]), byref(buflen))
+    if rc == 0:
+        for a in adapterList:
+            adNode = a.ipAddressList
+            while True:
+                try:
+                    ipAddr = adNode.ipAddress
+                    if ipAddr:
+                        yield ipAddr
+                    adNode = adNode.next
+                    if not adNode:
+                        break
+                except Exception:
+                    break
 
 class Matrix:
     packet_frames = 8
@@ -109,12 +165,11 @@ class Matrix:
 
     @staticmethod
     def get_local_ip():
-        from netifaces import interfaces, ifaddresses, AF_INET
-        for ifaceName in interfaces():
-            addresses = [i['addr'] for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr': ''}])]
-            print(addresses)
-            if Matrix.check_format_ip(addresses[0]):
-                return addresses[0]
+        for addr in getIPAddresses():
+            addr = addr.decode('utf8')
+            if Matrix.check_format_ip(addr):
+                return addr
+
         raise ValueError('Cannot determine local ip address')
         local_host = []
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
